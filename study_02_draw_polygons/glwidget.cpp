08db4aca2c349fe2cb7ea74e2a4143cec6a84934
jiayuanxu90 <jiayuanxu90@gmail.com>
2014-12-12 19:30:38 +0800

    draw polygons
    draw a triangle and a quad

--- study_02_draw_polygons/glwidget.cpp
+++ study_02_draw_polygons/glwidget.cpp
@@ -33,6 +33,24 @@ void GLWidget::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
+
+    glTranslatef(-1.5, 0.0, -6.0);
+
+    glBegin(GL_TRIANGLES);
+    glVertex3f(0.0, 1.0, 0.0);
+    glVertex3f(-1.0, -1.0, 0.0);
+    glVertex3f(1.0, -1.0, 0.0);
+    glEnd();
+
+    glTranslatef(3.0, 0.0, 0.0);
+
+    glBegin(GL_QUADS);
+    glVertex3f(-1.0, 1.0, 0.0);
+    glVertex3f(1.0, 1.0, 0.0);
+    glVertex3f(1.0, -1.0, 0.0);
+    glVertex3f(-1.0, -1.0, 0.0);
+    glEnd();
+
 }
 
 void GLWidget::resizeGL(int w, int h)
