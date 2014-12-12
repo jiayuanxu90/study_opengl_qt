27d59cc582061185f50921d33717a89c94c08c45
jiayuanxu90 <jiayuanxu90@gmail.com>
2014-12-12 18:54:08 +0800

    blank
    A blank framework uploaded.

---
+++ study_02_draw_polygons/glwidget.cpp
@@ -0,0 +1,60 @@
+#include "glwidget.h"
+#include <QtOpenGL>
+#include <QtWidgets>
+#include <gl/GLU.h>
+
+#include <iostream>
+using namespace std;
+
+GLWidget::GLWidget(QWidget *parent)
+    : QGLWidget(parent)
+{
+
+}
+
+GLWidget::~GLWidget()
+{
+
+}
+
+void GLWidget::initializeGL()
+{
+    glShadeModel(GL_SMOOTH);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+
+    glClearDepth(GL_DEPTH_TEST);
+
+    glDepthFunc(GL_LEQUAL);
+
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+}
+
+void GLWidget::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+}
+
+void GLWidget::resizeGL(int w, int h)
+{
+    if (h == 0) {
+        h = 1;
+    }
+    glViewport(0, 0, (GLint)w, (GLint)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0, (GLfloat)w/(GLfloat)h, 0.1, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ study_02_draw_polygons/study_02_draw_polygons.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-12-12T15:52:03
+#
+#-------------------------------------------------
+
+QT       += core gui opengl
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = study_02_draw_polygons
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        window.cpp \
+    glwidget.cpp
+
+HEADERS  += window.h \
+    glwidget.h
